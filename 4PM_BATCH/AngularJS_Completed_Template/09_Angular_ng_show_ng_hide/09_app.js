3a2573454634e7362792c0481408ba7625e3ea90
Naveen Saggam <thenaveensaggam@gmail.com>
2018-09-11 17:31:36 -0700

    removed

--- 4PM_BATCH/AngularJS_Completed_Template/09_Angular_ng_show_ng_hide/09_app.js
+++
@@ -1,9 +0,0 @@
-// Create a module
-var app = angular.module('HBSApp',[]);
-
-// Create a Controller
-app.controller('HBSAppCtrl',function() {
-    this.eating = null;
-    this.coding = null;
-    this.sleeping = null;
-});
